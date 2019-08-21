83abf071f9406eabc3cd654039e49691131fe086
Barath Arivazhagan <barathmacdec91@gmail.com>
2019-08-21 08:48:16 +0530

    reverted

--- src/main/java/com/barath/app/Application.java
+++ src/main/java/com/barath/app/Application.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class Application {
-
+	
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		SpringApplication.run(Application.class, args);
+	   SpringApplication.run(Application.class, args);
 	}
 
 }
